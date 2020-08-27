059e0cd76c28f4c33505bd16674e931f65e7a976
DerekHGK2001 <64647856+DerekHGK2001@users.noreply.github.com>
2020-08-26 21:09:55 -0600

    Add files via upload

--- ProyectoHolaMundo/Rueda.cpp
+++ ProyectoHolaMundo/Rueda.cpp
@@ -1,32 +1,32 @@
 #include "Rueda.h"
 #include <iostream>
 #include <string>
-#include <cstring>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
-using namespace std;
 
 double pi = 2 * acos(0.0);
 
-Rueda::Rueda(int target) :angulo(0), vuetas(3), targetF(target), background(al_load_bitmap("RuedaFilo.png"))
+Rueda::Rueda(int target) :angulo(0), vuetas(3), targetF(target), background(al_load_bitmap("RuedaFilo.png")), fin(false)
 {
-
+	background = al_load_bitmap("RuedaFilo.png");
 }
 
 void Rueda::Draw()
 {
+	Girar(targetF);
+	al_draw_rotated_bitmap(background, 201, 195, 250, 225, angulo, 0);
 
-	
 }
 
-
 void Rueda::setTargetF(float target) {
+	fin = false;
 	this->targetF = target;
 }
 
 void Rueda::Girar(float target)
 {
 	if (target != 0) {
+
 		if (this->vuetas != 0 || angulo < target) {
 			angulo += 4 * pi / 180;
 			if (angulo >= 6.2) {
@@ -34,12 +34,21 @@ void Rueda::Girar(float target)
 				angulo = 0;
 			}
 		}
+		else
+		{
+			vuetas = 3;
+			setTargetF(0);
+			angulo = 0;
+			fin = true;
+			al_rest(1);
+		}
 
 	}
 	else if (target >= angulo && vuetas == 0) {
 		vuetas = 3;
 		setTargetF(0);
 		angulo = 0;
+		fin = true;
 	}
 }
 
@@ -47,10 +56,10 @@ void Rueda::setAngulo(float _angulo) {
 	this->angulo = _angulo;
 }
 
-int Rueda::random(int max)// funcion randompara generar aleatoriedad
-{
-	int n;
-	srand(time(NULL));
-	n = rand();
-	return n % max;
+float Rueda::getAngulo() {
+	return this->angulo;
+}
+
+bool Rueda::getFin() {
+	return this->fin;
 }
--- ProyectoHolaMundo/Rueda.h
+++ ProyectoHolaMundo/Rueda.h
@@ -2,22 +2,19 @@
 #ifndef RUEDA_H
 #define RUEDA_H
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
 class Rueda {
 private:
 	float angulo, targetF;
 	int vuetas;
+	bool fin;
 	ALLEGRO_BITMAP* background;
-
-
 public:
 	Rueda(int);
 	void Draw();
 	void setTargetF(float);
 	void Girar(float);
 	void setAngulo(float);
-	int random(int);
+	float getAngulo();
+	bool getFin();
 };
-
 #endif // !RUEDA_H
